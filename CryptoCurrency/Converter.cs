71cde54c8ce5833a0d58b267f69c0965ec80f276
hertugweile <anders@vandret.dk>
2022-10-10 00:04:24 +0200

    Cryptocurrency

--- CryptoCurrency/Converter.cs
+++ CryptoCurrency/Converter.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CryptoCurrency
 {
     public class Converter
     {
+        public Dictionary<String, double> currencies = new Dictionary<String, double>();
+
         /// <summary>
         /// Angiver prisen for en enhed af en kryptovaluta. Prisen angives i dollars.
         /// Hvis der tidligere er angivet en værdi for samme kryptovaluta, 
@@ -13,7 +16,16 @@ namespace CryptoCurrency
         /// <param name="price">Prisen på en enhed af valutaen målt i dollars. Prisen kan ikke være negativ</param>
         public void SetPricePerUnit(String currencyName, double price)
         {
-
+            if (price < 0)
+            {
+                throw new InvalidOperationException("Prisen må ikke være mindre end nul");
+            } else {
+                if (currencies.ContainsKey(currencyName)) {
+                    currencies[currencyName] = price;
+                } else {
+                    currencies.Add(currencyName, price);
+                }
+            }
         }
 
         /// <summary>
@@ -27,7 +39,33 @@ namespace CryptoCurrency
         /// <returns>Værdien af beløbet i toCurrencyName</returns>
         public double Convert(String fromCurrencyName, String toCurrencyName, double amount) 
         {
-            return 0;
+            // BTC -> 10000;
+            // Litecoin -> 2500;
+
+            double value = 0;
+
+            try
+            {
+                value = currencies[fromCurrencyName] / currencies[toCurrencyName];
+            } catch (Exception ex)
+            {
+                throw new InvalidOperationException("Der er fejl i en af parametrene");
+            }
+            return value * amount;
         }
     }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cur = new Converter();
+            cur.SetPricePerUnit("BTC", -10000);
+            cur.SetPricePerUnit("LTE", 2500);
+            Console.WriteLine(cur.Convert("BTC", "LTE", 1)); // <--- Skal være 4
+        }
+    }
+
+
+
 }
